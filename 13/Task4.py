e10f7bea2b9712845adb7e4ed98ee7d0841b3926
MomusWinner <areg06.06@mail.ru>
2023-04-10 16:15:41 +0300

    lesson 13

---
+++ 13/Task4.py
@@ -0,0 +1,19 @@
+
+
+file = input("Type file name: ")
+words = {}
+maxValue = 0
+bestWord = ""
+
+with open(file,"r") as fr:
+    for i in fr:
+        for j in i.lower().split():
+            words[j] = i.split().count(j)
+
+for key in words:
+
+    if int(words[key]) >= maxValue:
+        maxValue = words[key]
+        bestWord = key
+
+print(f"Word: {bestWord} Quantity: {maxValue}")
